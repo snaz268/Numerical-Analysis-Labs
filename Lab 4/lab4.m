97fcce572d100032fa82b2640cde6798108a5536
Sana Naz <32523731+snaz268@users.noreply.github.com>
2019-02-19 21:49:04 +0500

    Create lab4.m

---
+++ Lab 4/lab4.m
@@ -0,0 +1,15 @@
+x_old = input('Enter guess:');
+tol = input('Enter tolerance:');
+syms x
+f = -12*x.^5 -6*x.^3 +10;
+iter = 0;
+for idx = 1:10
+   x_new = nr(f,x_old);
+   iter=iter+1;
+   err = abs((x_new - x_old)/x_new);
+   if( err < tol)
+       fprintf('Iteration %d: x=%.3f, err=%.3f\n', iter, x_new, err)
+       break
+   end
+   x_old = x_new;
+end
